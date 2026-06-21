e4d4d1dd948281a43a3aa4a5bd007a4fea838709
dvl <kamagaspar@gmail.com>
2026-06-21 03:21:32 -0400

    feat: implement category controller and service with create, findAll, and findOne methods

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { Category } from './entities/category.entity';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id') id: string,
+  ): Promise<Category | null> {
+    console.log(`Fetching category with ID: ${id}`);
+    return this.categoryService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() categoryData: CreateCategoryDto): Promise<Category> {
+    return this.categoryService.create(categoryData);
+  }
+
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+
+    constructor(
+        @InjectRepository(Category)
+        private readonly categoryRepository: Repository<Category>,
+    ) {}
+
+    findAll(): Promise<Category[]> {
+        return this.categoryRepository.find();
+    }
+
+    async findOne(id: string): Promise<Category | null> {
+        const category = await this.categoryRepository.findOneBy({ id });
+        if (!category) throw new NotFoundException(`Category with ID ${id} not found`);
+        return category;
+    }
+
+    async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+        const category = this.categoryRepository.create(createCategoryDto);
+        return this.categoryRepository.save(category);
+    }
+}
